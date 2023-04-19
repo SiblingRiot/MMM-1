55040ac26ec7d641cc2dfdad205e6c85d0079eb1
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-04-19 14:44:46 -0400

    Update MMM-1.js

--- MMM-1.js
+++ MMM-1.js
@@ -8,32 +8,53 @@
  */
 
 Module.register("MMM-1", {
-  // Default module config.
-  defaults: {
-    file: '/home/pi/MagicMirror/Connections/complimentsA.json',
-    messageType: 'anytime'
+  requiresVersion: "2.1.0",
+
+  start: function() {
+    this.message = "";
+    this.updateDom();
+
+    this.keyboard = this.createKeyboard();
+    this.keyboard.start();
   },
 
-  start: function () {
-    // Load the MMM-Keyboard module.
-    this.keyboard = MM.getModule("MMM-Keyboard");
-
-    // Add a keyboard input listener.
-    this.keyboard.on("input", (value) => {
-      // Append the input value to the file.
-      const fs = require("fs");
-      const path = require("path");
-      const filePath = path.resolve(__dirname, this.config.file);
-      const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-      data.push(value);
-      fs.writeFileSync(filePath, JSON.stringify(data));
-    });
-  },
-
-  // Override the getDom method to display the module content.
-  getDom: function () {
-    const wrapper = document.createElement("div");
-    wrapper.innerHTML = "My Module";
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.message;
     return wrapper;
+  },
+
+  createKeyboard: function() {
+    var keyboard = new Keyboard({
+      layoutName: "default",
+      inputElement: document.createElement("input"),
+      onKeyPress: this.onKeyPress.bind(this),
+    });
+    return keyboard;
+  },
+
+  onKeyPress: function(keyCode, event) {
+    if (keyCode === Keyboard.KEY_RETURN) {
+      this.saveMessage();
+      this.message = "";
+    } else if (keyCode === Keyboard.KEY_BACKSPACE) {
+      this.message = this.message.slice(0, -1);
+    } else {
+      this.message += event.key;
+    }
+    this.updateDom();
+  },
+
+  saveMessage: function() {
+    var fs = require("fs");
+    var path = require("path");
+    var message = {
+      text: this.message,
+      timestamp: new Date(),
+    };
+    var filePath = path.join("/home/pi/MagicMirror/Connections", "complimentsA.json");
+    var data = JSON.parse(fs.readFileSync(filePath));
+    data.push(message);
+    fs.writeFileSync(filePath, JSON.stringify(data));
   },
 });
