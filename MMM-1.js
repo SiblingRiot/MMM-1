2b1f395aebb2e4f0d2947376ee63c8beb99258d7
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-04-19 15:44:55 -0400

    Update MMM-1.js

--- MMM-1.js
+++ MMM-1.js
@@ -7,54 +7,80 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-1", {
-  requiresVersion: "2.1.0",
+Module.register('MMM-1', {
+  defaults: {
+    jsonFilePath: '/home/pi/MagicMirror/Connections/complimentsA.json',
+    messageType: 'anytime'
+  },
 
-  start: function() {
-    this.message = "";
-    this.updateDom();
-
-    this.keyboard = this.createKeyboard();
-    this.keyboard.start();
+  start: function () {
+    Log.info('MMM-Compliment-Sender started');
+    this.sendSocketNotification('LOAD_JSON_DATA', {...this.defaults});
   },
 
   getDom: function() {
+    var self = this;
     var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.message;
+    wrapper.innerHTML = "Type a message:";
+
+    var keyboard = document.createElement("div");
+    keyboard.setAttribute("class", "keyboard");
+    keyboard.innerHTML = "<div class='keys'></div>";
+    wrapper.appendChild(keyboard);
+
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "messageInput");
+    input.setAttribute("maxlength", "50");
+    input.setAttribute("placeholder", "Type your message here");
+    keyboard.insertBefore(input, keyboard.firstChild);
+
+    input.addEventListener("focus", function() {
+      self.sendNotification("KEYBOARD_SHOW");
+    });
+
+    input.addEventListener("blur", function() {
+      self.sendNotification("KEYBOARD_HIDE");
+    });
+
+    this.sendNotification("KEYBOARD_REGISTER", {
+      inputElement: input,
+      keyboardElement: keyboard.querySelector(".keys")
+    });
+
+    var button = document.createElement("button");
+    button.innerHTML = "Send";
+    button.addEventListener("click", () => {
+      var message = input.value.trim();
+      if (message.length > 0) {
+        this.sendSocketNotification('SAVE_MESSAGE', { ...this.defaults, type: this.config.messageType, message: message });
+        input.value = '';
+      }
+    });
+    wrapper.appendChild(button);
     return wrapper;
   },
 
-  createKeyboard: function() {
-    var keyboard = new Keyboard({
-      layoutName: "default",
-      inputElement: document.createElement("input"),
-      onKeyPress: this.onKeyPress.bind(this),
-    });
-    return keyboard;
-  },
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'SAVE_MESSAGE') {
+      // do something with the loaded JSON data if needed
+      const fs = require('fs');
+      fs.readFile("complimentsA.json", (err, data) => {  // READ
+        if (err) {
+          return console.error(err);
+      };
 
-  onKeyPress: function(keyCode, event) {
-    if (keyCode === Keyboard.KEY_RETURN) {
-      this.saveMessage();
-      this.message = "";
-    } else if (keyCode === Keyboard.KEY_BACKSPACE) {
-      this.message = this.message.slice(0, -1);
-    } else {
-      this.message += event.key;
+      var data = JSON.parse(data.toString());
+      data.age = "23"; // MODIFY
+      var writeData = fs.writeFile("complimentsA.json", JSON.stringify(data), (err, result) => {  // WRITE
+          if (err) {
+              return console.error(err);
+          } else {
+              console.log(result);
+              console.log("Success");
+          }
+      });
+  });
+      }
     }
-    this.updateDom();
-  },
-
-  saveMessage: function() {
-    var fs = require("fs");
-    var path = require("path");
-    var message = {
-      text: this.message,
-      timestamp: new Date(),
-    };
-    var filePath = path.join("/home/pi/MagicMirror/Connections", "complimentsA.json");
-    var data = JSON.parse(fs.readFileSync(filePath));
-    data.push(message);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-  },
 });
